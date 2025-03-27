f0e45056580ef3262b8707ebbbbfafd33ab83883
MohammadxAnas <mohammadanasqadar@gmail.com>
2025-03-27 23:53:23 +0530

    practice

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Navigation } from "./components/navigation";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +23,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <header>Practicing next js</header>
+        <header>
+          <Navigation/>
+        </header>
         {children}
         <footer>Practicing next js</footer>
       </body>
